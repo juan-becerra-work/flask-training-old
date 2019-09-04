ed539e1d7632f5010b58f4567dbc44367ffe6b2a
Juan Becerra <juan.becerrawork@gmail.com>
2019-09-04 17:28:17 -0300

    Se agregan comentarios al archivo de la aplicación

--- dockertest/dockertest.py
+++ dockertest/dockertest.py
@@ -6,13 +6,23 @@ import json
 app = Flask(__name__)
 
 
+#Declara la ruta raiz
 @app.route("/", methods=["GET", "POST"])
 def dockertest():
+
+    # Abre el archivo JSON con la respuesta de la API
     with open('apiResponse.json') as json_data:
+
+        # Carga el archivo JSON a una variable
         jsonResponse = json.load(json_data)
+
+        # Cierra el archivo
         json_data.close()
+
+    # Devuelve la respuesta de la URL ( http://localhost:5000)
     return (jsonResponse)
 
 
+# INICIO DE LA APLICACIÓN
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
